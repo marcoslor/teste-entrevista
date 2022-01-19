0e62769715c9868d58afc99b438e2b5d2b577f55
Marcos Rios <rioos.marcos@gmail.com>
2022-01-19 17:25:05 -0300

    Fixed path

--- src/Database/Migrations.php
+++ src/Database/Migrations.php
@@ -7,7 +7,7 @@ class Migrations
     // executa a query sql para criar as tabelas
     public static function run(): void
     {
-        $sql = file_get_contents(__DIR__ . '/../../docker/db/create_db.sql');
+        $sql = file_get_contents(__DIR__ . '/../../create_db.sql');
 
         Connection::getInstance()->exec($sql);
     }
